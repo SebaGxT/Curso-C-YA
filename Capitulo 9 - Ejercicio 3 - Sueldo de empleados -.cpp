a4c67d61f0e09604827d7f3cb893724d69886849
Sebastian Omar de la Vega <61508227+SebaGxT@users.noreply.github.com>
2020-07-01 00:02:50 -0300

    Rename Capitulo 9 - Ejercicio 3 -.cpp to Capitulo 9 - Ejercicio 3 - Sueldo de empleados -.cpp

--- Capitulo 9 - Ejercicio 3 - Sueldo de empleados -.cpp
+++ Capitulo 9 - Ejercicio 3 - Sueldo de empleados -.cpp
@@ -1,5 +1,5 @@
-/*En una empresa trabajan n empleados cuyos sueldos oscilan entre $100 y $500, realizar un programa que lea los sueldos que cobra cada empleado e informe cu·ntos empleados cobran entre $100 y $300 y cu·ntos cobran m·s de $300.
- Adem·s el programa deber· informar el importe que gasta la empresa en sueldos al personal.*/
+/*En una empresa trabajan n empleados cuyos sueldos oscilan entre $100 y $500, realizar un programa que lea los sueldos que cobra cada empleado e informe cu√°ntos empleados cobran entre $100 y $300 y cu√°ntos cobran m√°s de $300.
+ Adem√°s el programa deber√° informar el importe que gasta la empresa en sueldos al personal.*/
  //Librerias
 #include <stdio.h>
 #include <stdlib.h>
